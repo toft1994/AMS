49b81e09ed607d17d8f7ae3fc74e04d98c63cf2f
Mikkel Jensen <AU592741@uni.au.dk>
2020-02-27 11:05:01 +0100

    Added KeyPad class

---
+++ RobotSorter/RobotSorter/KeyPad/KeyPad.cpp
@@ -0,0 +1,8 @@
+/*
+ * KeyPad.cpp
+ *
+ * Created: 27-02-2020 11:01:52
+ *  Author: Mikkel
+ */ 
+#include "KeyPad.h"
+
---
+++ RobotSorter/RobotSorter/KeyPad/KeyPad.h
@@ -0,0 +1,15 @@
+/*
+ * KeyPad.h
+ *
+ * Created: 27-02-2020 11:02:07
+ *  Author: Mikkel
+ */ 
+
+
+#ifndef KEYPAD_H_
+#define KEYPAD_H_
+
+
+
+
+#endif /* KEYPAD_H_ */
