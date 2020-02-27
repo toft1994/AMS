0bd1fb3567046a340452f4c879f00e0883cf984d
Jesper Toft Jakobsen <jesper.toft.jakobsen@gmail.com>
2020-02-27 09:13:33 +0100

    Added projekt

---
+++ RobotSorter/RobotSorter/main.cpp
@@ -0,0 +1,18 @@
+/*
+ * RobotSorter.cpp
+ *
+ * Created: 27-02-2020 09:09:45
+ * Author : jespe
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
